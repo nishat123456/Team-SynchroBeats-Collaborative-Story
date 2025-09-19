1c6e316a0ae156287b0f3345898c0629ea0010c2
Sujjal Chapagain <rocksujal02@gmail.com>
2025-09-19 17:31:43 -0500

    Add chapter2.py

---
+++ chapter2.py
@@ -0,0 +1,16 @@
+# chapter_2.py
+
+
+# A new character, a knight named Sir Git, appeared from the shadows.
+
+# He carried a glowing sword, which hummed with the power of a thousand pull requests.
+
+
+def introduce_character():
+
+    print("Sir Git said, 'I have come to merge our destinies.'")
+
+    print("The blinking cursor responded, 'Let's do this on a new branch.'")
+
+
+introduce_character()
