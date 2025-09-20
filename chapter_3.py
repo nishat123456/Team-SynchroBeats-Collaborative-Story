b4350cca75acfd5450e0fb863033d970e1902bb2
Dikshant Aryal <iamdikshant0@gmail.com>
2025-09-19 20:12:16 -0500

    Create chapter_3.py
    Added New Chapter: Introducing new character: Lord Aryal

---
+++ chapter_3.py
@@ -0,0 +1,16 @@
+# chapter_3.py
+
+# *Cut to the next scene* 
+
+# Lound thunder echoing in his halls, bombarding with the walls, find the way to a respected coder Lord Aryal.
+
+# He takes a sip of Diet Coke after a long coding session
+
+
+def introduce_new_character():
+
+    print("Lord Aryal said, 'I can do this all day!'")
+    print("The thunder replies, 'For sure! My Lord.'")
+
+
+introduce_new_character()
